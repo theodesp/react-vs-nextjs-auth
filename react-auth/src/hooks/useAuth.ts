b37b86f255651fba7ecadd748c5ab334cf54fb60
theodesp <328805+theodesp@users.noreply.github.com>
2024-02-12 17:08:28 +0000

    Format useAuth

--- react-auth/src/hooks/useAuth.ts
+++ react-auth/src/hooks/useAuth.ts
@@ -1,15 +1,15 @@
-    import { useContext } from "react";
-    import { AuthContext } from "../providers/AuthProvider";
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
 
-    // Custom hook to consume AuthContext
-    export const useAuth = () => {
-    const authContext = useContext(AuthContext);
+// Custom hook to consume AuthContext
+export const useAuth = () => {
+  const authContext = useContext(AuthContext);
 
-    if (!authContext) {
-        throw new Error(
-        "useAuth hook was called outside of context, make sure your app is wrapped with AuthProvider"
-        );
-    }
+  if (!authContext) {
+    throw new Error(
+      "useAuth hook was called outside of context, make sure your app is wrapped with AuthProvider"
+    );
+  }
 
-    return authContext;
-    };
+  return authContext;
+};
